fd167768a82bd2a7f17b2cc02c47b1114dd3d94f
Ed Kelly <ekelly@sapient.com>
2018-05-19 13:49:25 +0100

    Remove generic header step def

--- features/step-definitions/headers.js
+++
@@ -1,6 +0,0 @@
-const { When } = require('cucumber');
-const headerStore = require('../support/store/header');
-
-When(/^I set the header (.*) to (.*)$/, (header, value) => {
-  headerStore.set(header, value);
-});
